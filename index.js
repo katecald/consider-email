ddfca71a146fafc7c0e76aa61b3a14df063c5d3e
Kate Caldwell <kate.kec.caldwell@gmail.com>
2019-01-04 16:14:57 -0500

    add date sort

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ process.stdin.on('data', () => {
   const emailFolderPath = process.argv[2]
   const emailFiles = fs.readdirSync(emailFolderPath)
 
-  const placeReplyInStore = (replyID, messageID) => {
+  const insertReplyIntoThread = (replyID, messageID, date) => {
     emailStore.some(thread => {
-      if (thread[thread.length-1] === replyID) {
-        thread.push(messageID)
+      if (thread[thread.length - 1].messageID === replyID) {
+        thread.push({ messageID, date })
         return true
       }
     })
@@ -20,21 +20,27 @@ process.stdin.on('data', () => {
 
     const findMessageIDMatch = /[\n\r].*Message-ID:\s*([^\n\r]*)/g
     const findReplyIDMatch = /[\n\r].*In-Reply-To:\s*([^\n\r]*)/g
+    const findDateMatch = /[\n\r].*Date:\s*([^\n\r]*)/g
 
     const messageIDMatch = findMessageIDMatch.exec(email)
     const replyIDMatch = findReplyIDMatch.exec(email)
+    const dateMatch = findDateMatch.exec(email)
 
     const messageID = messageIDMatch && messageIDMatch[1]
     const replyID = replyIDMatch && replyIDMatch[1]
+    const date = dateMatch && Date.parse(dateMatch[1])
 
     // If email is a reply, find existing thread and push it there
     if (replyID) {
-      placeReplyInStore(replyID, messageID)
-    // If email is net new, start new thread
+      insertReplyIntoThread(replyID, messageID, date)
+      // If email is net new, start new thread
     } else {
-      emailStore.push([messageID])
+      emailStore.push([{ messageID, date }])
     }
   })
+
+  // Sort the threads descending by most recent date
+  emailStore.sort((a, b) => b[b.length-1].date - a[a.length-1].date)
 
   console.log('Sorted Emails: ', emailStore)
 })
