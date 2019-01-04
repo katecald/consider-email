07298f102b0918118a87d33063a365092c137017
Kate Caldwell <kate.kec.caldwell@gmail.com>
2019-01-04 13:13:53 -0500

    sorted emailStore printing to cmd line

--- index.js
+++ index.js
@@ -1,28 +1,40 @@
-process.stdin.resume()
-process.stdin.setEncoding('utf8')
-
+const fs = require('fs')
+// Container for sorted emails
 const emailStore = []
 
+process.stdin.on('data', () => {
+  const emailFolderPath = process.argv[2]
+  const emailFiles = fs.readdirSync(emailFolderPath)
 
-process.stdin.on('data', email => {
-  const findMessageIDMatch = /[\n\r].*Message-ID:\s*([^\n\r]*)/g
-  const findReplyIDMatch = /[\n\r].*In-Reply-To:\s*([^\n\r]*)/g
-  
-  const messageIDMatch = findMessageIDMatch.exec(email)
-  const replyIDMatch = findReplyIDMatch.exec(email)
-
-  const messageID = messageIDMatch && messageIDMatch[1]
-  const replyID = replyIDMatch && replyIDMatch[1]
-
-  console.log('messageID', messageID, 'replyID', replyID)
-
-  if (replyID) {
-    console.log('reply', replyID)
-  } else {
-    emailStore.push([messageID])
+  const placeReplyInStore = (replyID, messageID) => {
+    emailStore.some(thread => {
+      if (thread[thread.length-1] === replyID) {
+        thread.push(messageID)
+        return true
+      }
+    })
   }
-})
 
-process.stdin.on('end', function() {
-  console.log('done', emailStore)
+  emailFiles.forEach(emailFile => {
+    const email = fs.readFileSync(`${emailFolderPath}/${emailFile}`)
+
+    const findMessageIDMatch = /[\n\r].*Message-ID:\s*([^\n\r]*)/g
+    const findReplyIDMatch = /[\n\r].*In-Reply-To:\s*([^\n\r]*)/g
+
+    const messageIDMatch = findMessageIDMatch.exec(email)
+    const replyIDMatch = findReplyIDMatch.exec(email)
+
+    const messageID = messageIDMatch && messageIDMatch[1]
+    const replyID = replyIDMatch && replyIDMatch[1]
+
+    // If email is a reply, find existing thread and push it there
+    if (replyID) {
+      placeReplyInStore(replyID, messageID)
+    // If email is net new, start new thread
+    } else {
+      emailStore.push([messageID])
+    }
+  })
+
+  console.log('Sorted Emails: ', emailStore)
 })
